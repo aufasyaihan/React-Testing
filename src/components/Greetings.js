23a69b630ff3ba33274067153a287fbc70754e56
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-07-09 23:06:05 +0700

    added state management

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
+
 const Greetings = () => {
-    return (
-        <div>
-            <h1>Hello, World!</h1>
-        </div>
-    );
+  const [changeText, setChangeText] = useState(false);
+  const changeTextHandler = () => {
+    setChangeText(true);
+  };
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      {changeText ? <p>Changed!</p> : <p>Not Changed!</p>}
+      <button onClick={changeTextHandler}>Change Text</button>
+    </div>
+  );
 };
 
 export default Greetings;
