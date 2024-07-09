2dc1e7ef4abf45e8f06355b1bad1a72af3411c9a
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-07-09 23:13:08 +0700

    added click event test

--- src/components/Greetings.test.js
+++ src/components/Greetings.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Greetings from "./Greetings";
+import userEvent from "@testing-library/user-event";
 
 describe("Greetings", () => {
   test("Hello Word is rendered", () => {
@@ -14,5 +15,30 @@ describe("Greetings", () => {
       exact: false,
     }); // Get the element containing the text "Hello, World!" that is not case sensitive
     expect(helloWorldElemet).toBeInTheDocument(); // Assert that the element is in the document
+  });
+
+  test("Not Changed! is rendered", () => {
+    // Arrange
+    render(<Greetings />);
+
+    // Act
+    // ...nothing
+
+    // Assert
+    const outputElement = screen.getByText("Not Changed!");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("Changed! is rendered after button click", () => {
+    // Arrange
+    render(<Greetings />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement); // Simulate a click event on the button
+
+    // Assert
+    const changedElement = screen.getByText("Changed!");
+    expect(changedElement).toBeInTheDocument();
   });
 });
