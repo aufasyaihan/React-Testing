ea0fc0a346b42d123a6cde23a8e3992579bd26af
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-07-09 23:03:28 +0700

    Added grouped test

--- src/components/Greetings.test.js
+++ src/components/Greetings.test.js
@@ -1,14 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import Greetings from "./Greetings";
 
-test("Hello Word is rendered", () => {
-  // Arrange
-  render(<Greetings />);
+describe("Greetings", () => {
+  test("Hello Word is rendered", () => {
+    // Arrange
+    render(<Greetings />);
 
-  // Act
-  // ...nothing
+    // Act
+    // ...nothing
 
-  // Assert
-  const helloWorldElemet = screen.getByText("Hello, World!", { exact: false }); // Get the element containing the text "Hello, World!" that is not case sensitive
-  expect(helloWorldElemet).toBeInTheDocument(); // Assert that the element is in the document
+    // Assert
+    const helloWorldElemet = screen.getByText("Hello, World!", {
+      exact: false,
+    }); // Get the element containing the text "Hello, World!" that is not case sensitive
+    expect(helloWorldElemet).toBeInTheDocument(); // Assert that the element is in the document
+  });
 });
