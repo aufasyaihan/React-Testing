ac119f2f3ba90efe9acccf9d92896083701e7810
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-07-09 23:47:41 +0700

    using mock function

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -4,12 +4,18 @@ import Async from "./Async";
 describe("Async Component", () => {
   test("renders posts if request succeeds", async () => {
     // Arrange
+    // Mock the fetch function to return a resolved promise with the specified data
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: 1, title: "First Post" }],
+    });
     render(<Async />);
 
     // Act
     // ...nothing
 
     // Assert
+    // Find all list items and assert that the length is not zero
     const listItems = await screen.findAllByRole("listitem");
     expect(listItems).not.toHaveLength(0);
   });
